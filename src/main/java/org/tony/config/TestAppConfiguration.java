92a9f5aa7c9e820c6e6082e5dba997099bf1f2a8
zagao <zan.gao@oracle.com>
2019-10-30 15:16:10 +0800

    Use lombok to simplify the code.

--- src/main/java/org/tony/config/TestAppConfiguration.java
+++ src/main/java/org/tony/config/TestAppConfiguration.java
@@ -1,27 +1,15 @@
 package org.tony.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "greeting")
+@Getter
+@Setter
 public class TestAppConfiguration {
    private String template;
    private String defaultName;
-
-    public String getTemplate() {
-        return template;
-    }
-
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    public void setDefaultName(String defaultName) {
-        this.defaultName = defaultName;
-    }
 }
