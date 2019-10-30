a54767d7503964a1a883fbeccb4c81f4c25a9d45
zagao <zan.gao@oracle.com>
2019-10-30 14:20:32 +0800

    Building a RESTful Web Service.

---
+++ src/main/java/org/tony/controller/HelloContoller.java
@@ -0,0 +1,12 @@
+package org.tony.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContoller {
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+}
